0379a1dcf53d65e90dba1138806207e34a3c1cb7
Korey Peterson <korey.t.peterson@gmail.com>
2019-11-10 18:53:25 -0600

    add button is functional

--- client.js
+++ client.js
@@ -1,14 +1,14 @@
 $( document ).ready(onReady);
 
 function onReady() {
-    $('#submit-button').on('click', putInputInArray );
+    $('#sub-btn').on('click', putInputInArray );
     console.log( ' hiya' );
     showEmployees(employeeArray);    
 }
 
 // This array is for when we use the form and it will store here.
-// const employeeArray = [{firstName, lastName, id, title, annualSalary}];
     let employeeArray = [];
+    // let employeeArray = [];
 
 //take input from the form
 function putInputInArray(event) {
@@ -17,19 +17,17 @@ function putInputInArray(event) {
     event.preventDefault();
 
     //define values
-    let firstName = $('#first-name-input').val();
+    let firstName = $("#first-name-input").val();
     let lastName = $('#last-name-input').val();
     let id = $('#id-input').val();
     let title = $('#title-input').val();
     let annualSalary = $('#annual-salary-input').val();
-    let newEmployee = {firstName, lastName, id, title, annualSalary} ;
-    console.log('my employee is:', employeeArray ) ;
+    let newEmployee = {firstName, lastName, id, title, annualSalary};
+    console.log(newEmployee);
     
     //push inputs into array
     employeeArray.push(newEmployee);
     console.log('employee array:', employeeArray );
-    //call function to DOM 
-    show(employeeArray);
 
 // clear out our input values from the form
     $('#first-name-input').val('');
@@ -37,18 +35,21 @@ function putInputInArray(event) {
     $('#id-input').val('');
     $('#title-input').val('');
     $('#annual-salary-input').val('');
+
+    //call function to DOM 
+    showEmployees(employeeArray);
 }
 // show employees entered in the dom
 function showEmployees(employeeList) {
-    $('#movie-table').empty();
+    $('#employee-table').empty();
     for( let employee of employeeList ) {
-        $('#movie-table').append(`
+        $('#employee-table').append(`
         <tr>
             <td>${employee.firstName}</td>
-            <td>${emloyee.lastName}</td>
-            <td>${emloyee.id}</td>
-            <td>${emloyee.title}</td>
-            <td>${emloyee.annualSalary}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.id}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
             <td> <button class="delete">Delete</button> </td>
         </tr>
         `)
